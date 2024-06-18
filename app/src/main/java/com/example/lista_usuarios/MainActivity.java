bce176f4d6d29f9e81a442cb5e10f71e8a37ae7b
DonatoConsuegra <dconsuegrag2@uteq.edu.ec>
2024-06-18 09:38:50 -0500

    save

--- app/src/main/java/com/example/lista_usuarios/MainActivity.java
+++ app/src/main/java/com/example/lista_usuarios/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.lista_usuarios;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+
+import com.example.lista_usuarios.WebService.WebService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +23,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        lstOpciones = (ListView)findViewById(R.id.main);
+        View header = getLayoutInflater().inflate(R.layout.layout_item, null);
+        lstOpciones.addHeaderView(header);
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws= new WebService("https://reqres.in/api/users",
+                datos, MainActivity.this, MainActivity.this);
+        ws.execute("GET");
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
